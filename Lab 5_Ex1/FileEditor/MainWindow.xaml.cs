77c90553d61988481c4c91ff9c244ea7706b08f8
nytackoe <107689065+nytackoe@users.noreply.github.com>
2022-06-17 11:52:46 -0700

    Add files via upload

---
+++ Lab 5_Ex1/FileEditor/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace FileEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Name of file in use
+        /// </summary>
+        private string fileName = String.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Open the file after the user has been prompted for the file name
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO - Update the OpenButton_Click method
+            // Call GetFileName to get the name of the file to load 
+            fileName = GetFileName(); 
+
+            
+            // Populate the editor TextBox with the file contents
+            if (fileName != String.Empty)
+            {
+                editor.Text =
+                TextFileOperations.ReadTextFileContents(fileName);
+            }
+         }
+
+        // TODO - Implement a method to get the file name
+        // Add a GetFileName method
+
+        private string GetFileName()
+        {
+            string fname = String.Empty;
+            OpenFileDialog openFileDlg = new OpenFileDialog();
+            openFileDlg.InitialDirectory =
+            @"E:\Labfiles\Lab 5\Ex1\Starter";
+            openFileDlg.DefaultExt = ".txt";
+            openFileDlg.Filter = "Text Documents (.txt)|*.txt";
+
+            bool? result = openFileDlg.ShowDialog();
+            if (result == true)
+            {
+                fname = openFileDlg.FileName;
+            }
+            return fname;
+        }
+ 
+
+        // Save the data back to the file
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO - Update the SaveButton_Click method
+            // Write the contents of the editor TextBox back to the file
+
+            if (fileName != String.Empty)
+            {
+                TextFileOperations.WriteTextFileContents(fileName, editor.Text);
+            }
+        }
+    }   
+}
